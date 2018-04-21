50f7ba058c110b8c490c16f0e512d3060ba7e049
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-04-21 00:49:27 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,10 @@
 def reformat_languages(languages)
-  # your code here
+  new = {}
+  new_hash = languages.each do |k,v|
+    v.each do |data, values|
+      #puts "DATA = #{data} and VALUES = #{values}"
+      values[:style] = [k]
+    end
+  end
+ new_hash.reduce({}, :merge) 
 end
